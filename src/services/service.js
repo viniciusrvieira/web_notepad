6c42481833b4228f2e387aec5b21f59480086d09
viniciusrvieira <viniciusr.vieira@hotmail.com>
2022-08-19 18:01:51 -0300

    Update: added xml support

--- src/services/service.js
+++ src/services/service.js
@@ -1,8 +1,14 @@
 const controller = require('../controllers/controller');
+const { XMLBuilder } = require('fast-xml-parser');
+const builder = new XMLBuilder();
 
 function readPath(req, res) {
   const { query } = req;
   const pathContent = controller.readPath(`${query.url}`);
-  res.send(pathContent);
+  if (query.format && query.format == 'xml')
+    return res
+      .set('Content-Type', 'application/xml')
+      .send(builder.build(pathContent));
+  res.set('Content-Type', 'application/json').send(pathContent);
 }
 module.exports = { readPath };
